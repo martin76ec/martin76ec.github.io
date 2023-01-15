8c95a12b0cbbd2ed583f1a04239e0c380d0cc3e9
martin76ec <larreaamartin@gmail.com>
2023-01-15 03:32:24 -0500

    feat: add astro config

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@components': resolve(__dirname, './src/components'),
+        '@hooks': resolve(__dirname, 'src/hooks'),
+        '@layouts': resolve(__dirname, './src/layouts'),
+        '@pages': resolve(__dirname, 'src/pages'),
+        '@public': resolve(__dirname, './public'),
+      }
+    }
+  }
+});
