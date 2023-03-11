04267796dae2ca8c2ee695fd446c97673fdba576
martin76ec <larreaamartin@gmail.com>
2023-03-11 03:26:35 -0500

    feat: refactor timeline

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,114 +1,58 @@
-import './style.scss';
-
-function Timeline() {
+function Year({ children }) {
   return (
-    <div className='relative'>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="main-timeline">
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2018</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-rocket" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">Web Development</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-users" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">JavaScript</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-cog" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">Mongo DB</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-heart" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">BlackPearl</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-globe" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">Angular</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-apple" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">Laravel</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-              <div className="timeline">
-                <a href="#" className="timeline-content">
-                  <span className="timeline-year">2017</span>
-                  <div className="timeline-icon">
-                    <i className="fa fa-edit" aria-hidden="true"></i>
-                  </div>
-                  <div className="content">
-                    <h3 className="title">Creapure</h3>
-                    <p className="description">
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </p>
-                  </div>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className={`flex justify-center items-center w-full text-[100px]`}>
+      <p className='font-black text-white'>{children}</p>
+    </div>
+  );
+}
+
+function Divider() {
+  return (
+    <div className='flex justify-center w-full'>
+      <div className='w-1 bg-white h-full' />
+    </div>
+  );
+}
+
+function Description({ children }) {
+  return (
+    <div className='flex justify-center items-center text-[30px]'>
+      <p className='text-4xl text-white'>{children}</p>
+    </div>
+  );
+}
+
+function Timestamp({ children, reverse }) {
+  const { year, description } = children;
+
+  return (
+    <div className='grid grid-cols-3 w-[80%] h-[80vh]'>
+      {reverse && <Year>{year}</Year>}
+      {!reverse && <Description>{description}</Description>}
+      <Divider />
+      {reverse && <Description>{description}</Description>}
+      {!reverse && <Year>{year}</Year>}
+    </div>
+  );
+}
+
+function Connector() {
+  return <div className='w-2 h-6 bg-white rounded-full' />;
+}
+
+function Timeline({ milestones }) {
+  return (
+    <div className='flex flex-col items-center w-full gap-20'>
+      <div className='w-10 h-10 bg-white rounded-full' />
+      {
+        milestones.map((milestone, i) => (
+          <>
+            <Timestamp reverse={i % 2 === 0}>{milestone}</Timestamp>
+            {i < milestones.length - 1 && <Connector />}
+          </>
+        ))
+      }
+      <div className='flex justify-center items-center w-10 h-10 border-2 border-white rounded-full z-2 after:content-[""] after:w-5 after:h-5 after:bg-white after:rounded-full' />
     </div>
   );
 }
