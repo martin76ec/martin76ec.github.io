9c31205e4e817ed8b2500a416d2fb846699baac8
martin76ec <larreaamartin@gmail.com>
2023-06-12 01:58:35 -0500

    feat: add prettier config feat: add millionjs

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,33 @@
-import { defineConfig } from 'astro/config';
+import million from "million/compiler";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "astro/config";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    config: {
-      path: './tailwind.config.ts'
-    }
-  })],
-  site: 'https://martin76ec.github.io',
+  integrations: [
+    react(),
+    tailwind({
+      config: {
+        path: "./tailwind.config.ts",
+      },
+    }),
+  ],
+  site: "https://martin76ec.github.io",
   vite: {
     resolve: {
       alias: {
-        '@components': resolve(__dirname, './src/components'),
-        '@hooks': resolve(__dirname, 'src/hooks'),
-        '@layouts': resolve(__dirname, './src/layouts'),
-        '@pages': resolve(__dirname, 'src/pages'),
-        '@assets': resolve(__dirname, 'src/assets')
-      }
-    }
-  }
+        "@components": resolve(__dirname, "./src/components"),
+        "@hooks": resolve(__dirname, "src/hooks"),
+        "@layouts": resolve(__dirname, "./src/layouts"),
+        "@pages": resolve(__dirname, "src/pages"),
+        "@assets": resolve(__dirname, "src/assets"),
+      },
+    },
+    plugins: [million.vite({ mode: "react", server: true })],
+  },
 });
