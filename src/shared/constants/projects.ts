e7c0636bd70c7b0c4d4f603b400843fa841b16b2
martin76ec <larreaamartin@gmail.com>
2024-08-14 15:20:00 -0500

    fix: add and replaced images

--- src/shared/constants/projects.ts
+++ src/shared/constants/projects.ts
@@ -1,6 +1,7 @@
 import Acodear from "@lib/assets/acodear.png";
 
-export interface Project { name: string;
+export interface Project {
+  name: string;
   image: string;
   link: string;
   description: string;
@@ -11,8 +12,7 @@ export const projects = [
   {
     name: "Acodear",
     image: Acodear.src,
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos aut eligendi reprehenderit odit facilis dolorum molestias aliquid quam sunt nesciunt ducimus debitis unde nobis blanditiis quod, neque enim id impedit?",
+    description: "Platform to learn to code, with video instructions, hints and a built-in code editor.",
     link: "https://dev.acodear.io",
     skills: ["Typescript", "Python", "AWS Amplify", "AWS Lambda", "Docker"],
   },
