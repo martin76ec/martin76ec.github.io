92898b4ee1b5039ca3dd2cf7a3856c7548b3843d
martin76ec <larreaamartin@gmail.com>
2025-01-24 03:19:36 -0500

    fix: add scroll percentage hook

--- src/shared/hooks/use-scroll.ts
+++ src/shared/hooks/use-scroll.ts
@@ -2,7 +2,12 @@
 export function useScroll(target: string, callback?: (...p: any) => any) {
   return () => {
     const _target = document.getElementById(target);
-    _target?.scrollIntoView({ behavior: "smooth" });
+    const scrollable = document.getElementById("scroll-content");
+    scrollable?.scrollBy({
+      top: _target?.getBoundingClientRect().y,
+      behavior: "smooth",
+    });
+    // _target?.scrollIntoView({ behavior: "smooth" });
     if (callback) callback();
   };
 }
