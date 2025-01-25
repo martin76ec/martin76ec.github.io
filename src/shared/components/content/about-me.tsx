2ea441c3f351fbb8ea2a7651d29596f7d7ee516e
martin76ec <larreaamartin@gmail.com>
2025-01-25 03:07:51 -0500

    feat: add email copy

--- src/shared/components/content/about-me.tsx
+++ src/shared/components/content/about-me.tsx
@@ -1,12 +1,49 @@
 import { Icon } from "@components/icon";
 import { NavMenu } from "@components/nav-menu/nav-menu";
+import { Button } from "@components/ui/button";
 import { authorDesc, authorName, authorRole } from "@constants/aboutme";
 import { cn } from "@lib/utils";
 import { useStore } from "@nanostores/react";
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 import { langStore } from "src/shared/stores/lang-store";
 
 interface Props {
   className?: string;
+}
+
+function Email() {
+  const [copiedText, setCopiedText] = useState("");
+
+  const handleCopy = async (text: string) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopiedText(text);
+      toast.success("Email copied to the clipboard :D");
+
+      setTimeout(() => {
+        setCopiedText("");
+      }, 3000);
+    });
+  };
+
+  return (
+    <div className="bg-muted-accent/50 flex max-w-96 items-center justify-between gap-2 rounded-lg border border-accent p-2 text-muted-foreground backdrop-blur-2xl">
+      <code className="rounded p-1 text-xs">martin.elarrea27@gmail.com</code>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => handleCopy("martin.elarrea27@gmail.com")}
+        className="h-6 w-6 border-muted/20 bg-accent"
+      >
+        {copiedText === "martin.elarrea27@gmail.com" ? (
+          <Check className="text-muted-background h-4 w-4 animate-spin-once" />
+        ) : (
+          <Copy className="text-muted-background h-4 w-4 animate-spin-once" />
+        )}
+      </Button>
+    </div>
+  );
 }
 
 export function AboutMe({ className }: Props) {
@@ -25,13 +62,14 @@ export function AboutMe({ className }: Props) {
           <p className="w-full text-lg text-muted-foreground lg:max-w-md">{authorDesc[lang]}</p>
         </div>
       </div>
-      <div className="flex gap-4 fill-muted-foreground">
+      <div className="flex items-center gap-4 fill-muted-foreground">
         <a href="https://www.linkedin.com/in/martinlarrea/" target="_blank" rel="noreferrer">
           <Icon name="linkedin" className="h-8 w-8 fill-muted-foreground hover:fill-foreground" />
         </a>
         <a href="https://github.com/martin76ec" target="_blank" rel="noreferrer">
           <Icon name="github" className="h-8 w-8 fill-muted-foreground hover:fill-foreground" />
         </a>
+        <Email />
       </div>
     </div>
   );
